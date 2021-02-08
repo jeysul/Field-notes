f8979e0b6ee7dd0131fb43b43488f521b23a6f44
sultan ahmed <65736680+jeysul@users.noreply.github.com>
2021-02-08 12:06:10 +0530

    Add files via upload

---
+++ Src/components/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLjs0T_EKHZdhFulKr5wLTGP5BivpVRTc",
+    authDomain: "field-notes-a5d50.firebaseapp.com",
+    databaseURL: "https://field-notes-a5d50.firebaseio.com",
+    projectId: "field-notes-a5d50",
+    storageBucket: "field-notes-a5d50.appspot.com",
+    messagingSenderId: "636733990429",
+    appId: "1:636733990429:web:31b5bacae0c98eb39cc8f4",
+    measurementId: "G-8QSEMKZ0XD"
+  };
---
+++ Src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+//import ReactDom from 'react-router-dom';
+
+const Home = ( ) =>{
+    return(
+        <>
+        <h1>Welcome Here Save Notes Easily</h1>
+        </>
+    );
+};
+
+export default Home;
